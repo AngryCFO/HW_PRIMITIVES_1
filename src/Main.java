2c60dbb03d9a51f8ae66db081d6cf57bd3c43fcf
AngryCFO <a79955700980@gmail.com>
2024-08-23 23:46:09 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Бонусные мили:");
         Case1.main(args);
-        System.out.println("\nБонус за пополнение:");
-        Case2.main(args);
+
     }
 }
